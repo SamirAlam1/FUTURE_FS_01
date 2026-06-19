750aee63728cef75d3db2f31ba3ac317e05cdbd4
Samir Alam <sa0409716@gmail.com>
2026-06-19 15:52:50 +0530

    Delete backend/routes/blogs.js

--- backend/routes/blogs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getBlogs, getBlog, createBlog, updateBlog, deleteBlog } = require('../controllers/blogController');
-const { protect } = require('../middleware/auth');
-
-router.route('/').get(getBlogs).post(protect, createBlog);
-router.route('/:id').get(getBlog).put(protect, updateBlog).delete(protect, deleteBlog);
-
-module.exports = router;
